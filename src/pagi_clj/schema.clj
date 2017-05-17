604333e4eb84fd878a1f50205b8280c0e5c49094
Kenneth Graham <kenneth.graham@digitalreasoning.com>
2017-05-17 14:59:49 -0500

    Finished schema loader

--- src/pagi_clj/schema.clj
+++ src/pagi_clj/schema.clj
@@ -16,12 +16,20 @@
   (let [is-node-type? #(= (:tag % nil) :nodeType)]
     (filter is-node-type? schema)))
 
-; all span nodes will have a :first and :last
+; all span nodes will have a :start and :length to reference the star and end
+; offsets into the source text
 (defn is-span? [node]
   "True if node is a span"
   (let [tags (:content node)
         has-span-tag? (not (empty? (filter #(= (:tag % nil) :span) tags)))]
     has-span-tag?))
+
+; all span container nodes will have a :first and :last
+(defn is-span-container? [node]
+  "True if node is a span"
+  (let [tags (:content node)
+        has-span-container-tag? (not (empty? (filter #(= (:tag % nil) :spanContainer) tags)))]
+    has-span-container-tag?))
 
 ; All sequence nodes will have a :next
 (defn is-sequence? [node]
@@ -39,16 +47,46 @@
         prop-list (not-empty (filter #(= (:tag % nil) property-type) tags))
         properties (map :attrs prop-list)
         named-properties (map #(->NamedProperty property-type (:name %) (dissoc % :name)) properties)]
-    (vec named-properties)))
+    (into [] named-properties)))
+
+(defn get-enum-properties [node]
+  "Returns enum properties, if present. Returns nil otherwise"
+  (let [tags (:content node)
+        properties (not-empty (filter #(= (:tag % nil) :enumProperty) tags))
+        attributes (-> properties first :attrs)
+        get-nested-enum #(-> % :attrs :name)
+        enums (->> properties
+                   first
+                   :content
+                   rest
+                   (map get-nested-enum)
+                   (into #{}))]
+    (if (empty? enums)
+      []
+      [(->NamedProperty :enumProperty
+                       (:name attributes)
+                       (assoc (dissoc attributes :name) :enums enums))]) ))
+
+(defrecord EdgeSpec [target-node-type min-arity max-arity])
+
+(defn- create-edge-spec [edge-def]
+  (let [{target-node-type :targetNodeType name :name min-arity :minArity max-arity :maxArity} edge-def]
+    {:name name :edge (->EdgeSpec target-node-type min-arity max-arity)}))
+
+(defn get-edge-specs [node]
+  (let [tags (:content node)
+        edge-specs (not-empty (filter #(= (:tag % nil) :edgeType) tags))
+        edge-specs (map #(create-edge-spec (:attrs %)) edge-specs)]
+    edge-specs))
 
 (defn get-node-type [node]
   "Return the type of the node, e.g. TOK or EXTRACTOR"
   (-> node :attrs :name))
 
-(defn create-node [is-sequence is-span
-                   string-properties int-properties float-properties])
+(defn get-id-generator [node]
+  (-> node :attrs :idGenerator))
 
-(defn create-node-generator [node]
+(defn node-spec-generator [node]
   "Given a node specification, generate a function to construct node. Node are
   specified like:
 
@@ -62,4 +100,18 @@
             {:tag :floatProperty, :attrs {:maxRange \"1.0\", :minRange \"0.0\", :maxArity \"1\", :minArity \"0\", :name \"margin\"}, :content nil}
             {:tag :edgeType, :attrs {:maxArity \"1\", :minArity \"0\", :targetNodeType \"RANKED_PREDICTION\", :name \"rankEdge\"}, :content nil}]"
 
-  )
+  {:is-span (is-span? node)
+   :is-span-container (is-span-container? node)
+   :is-sequence (is-sequence? node)
+   :node-type          (get-node-type node)
+   :id-generator (get-id-generator node)
+   :string-properties  (get-node-properties node :stringProperty)
+   :integer-properties (get-node-properties node :integerProperty)
+   :float-properties   (get-node-properties node :floatProperty)
+   :boolean-properties (get-node-properties node :booleanProperty)
+   :enum-properties    (get-enum-properties node)
+   :edges              (get-edge-specs node)})
+
+(defn create-node-specs [node-defs]
+  (into {}
+        (map #(hash-map (get-node-type %) (node-spec-generator %)) node-defs)))
